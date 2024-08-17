527c5447eb7c44862c67204517dfe612ceaf55cf
Jesse Anderson <jesse.anderson@gmail.com>
2024-08-17 09:50:07 -0700

    compatibility update

--- wofs/terrain.py
+++ wofs/terrain.py
@@ -22,14 +22,20 @@ def _shade_row(shade_mask, elev_m, sun_alt_deg, pixel_scale_m, no_data, fuzz=0.0
 
     # pure terrain angle shadow
     shade_mask[0] = LIT
-    shade_mask[1:] = numpy.where((elev_m[:-1] - elev_m[1:]) / pixel_scale_m < tan_sun_alt, LIT, SHADED)
+    shade_mask[1:] = numpy.where(
+        (elev_m[:-1] - elev_m[1:]) / pixel_scale_m < tan_sun_alt, LIT, SHADED
+    )
 
     # project shadows from tips (light->shadow transition)
     switch = numpy.where(shade_mask[:-1] != shade_mask[1:])
-    for i in switch[0]:  # note: could use flatnonzero instead of where; or else switch,=; to avoid [0]. --BL
+    for i in switch[
+        0
+    ]:  # note: could use flatnonzero instead of where; or else switch,=; to avoid [0]. --BL
         if shade_mask[i] == LIT:
             # TODO: horizontal fuzz?
-            shadow_level = (elev_m[i] + fuzz) - numpy.arange(shade_mask.size - i) * (tan_sun_alt * pixel_scale_m)
+            shadow_level = (elev_m[i] + fuzz) - numpy.arange(shade_mask.size - i) * (
+                tan_sun_alt * pixel_scale_m
+            )
             shade_mask[i:][shadow_level > elev_m[i:]] = SHADED
 
     shade_mask[elev_m == no_data] = UNKNOWN
@@ -55,14 +61,16 @@ def vector_to_crs(point, vector, original_crs, destination_crs):
     transformed_point, _ = transformed_line.points
 
     # take difference (i.e. remove origin offset)
-    transformed_vector = tuple(map(lambda x: x[1] - x[0], zip(*transformed_line.points)))
+    transformed_vector = tuple(
+        map(lambda x: x[1] - x[0], zip(*transformed_line.points))
+    )
     return transformed_point, transformed_vector
 
 
 def solar_vector(point, time, crs):
-    (lon, lat), (dlon, dlat) = vector_to_crs(point, (0, 100),
-                                             original_crs=CRS(crs),
-                                             destination_crs=CRS('EPSG:4326'))
+    (lon, lat), (dlon, dlat) = vector_to_crs(
+        point, (0, 100), original_crs=CRS(crs), destination_crs=CRS("EPSG:4326")
+    )
 
     # azimuth north to east of the vertical direction of the crs
     vert_az = math.atan2(dlon * math.cos(math.radians(lat)), dlat)
@@ -114,7 +122,7 @@ def shadows_and_slope(tile, time, no_data=-1000):
     norm_len = numpy.sqrt((xgrad * xgrad) + (ygrad * ygrad) + 1.0)
     slope = numpy.degrees(numpy.arccos(1.0 / norm_len))
 
-    x, y = tile.dims.keys()
+    y, x = tile.dims.keys()
     tile_center = (tile[x].values[x_size // 2], tile[y].values[y_size // 2])
     solar_vec = solar_vector(tile_center, to_datetime(time), tile.crs)
     sia = (solar_vec[2] - (xgrad * solar_vec[0]) - (ygrad * solar_vec[1])) / norm_len
@@ -122,29 +130,46 @@ def shadows_and_slope(tile, time, no_data=-1000):
 
     rot_degrees = 90.0 + math.degrees(solar_vec[3])
 
-    buff_elv_array = numpy.pad(tile.elevation.values, 4, mode='edge')
-    rotated_elv_array = ndimage.interpolation.rotate(buff_elv_array,
-                                                     rot_degrees,
-                                                     reshape=True,
-                                                     output=numpy.float32,
-                                                     cval=no_data,
-                                                     prefilter=False)
+    buff_elv_array = numpy.pad(tile.elevation.values, 4, mode="edge")
+    rotated_elv_array = ndimage.interpolation.rotate(
+        buff_elv_array,
+        rot_degrees,
+        reshape=True,
+        output=numpy.float32,
+        cval=no_data,
+        prefilter=False,
+    )
 
     # create the shadow mask by ray-tracying along each row
     shadows = numpy.zeros_like(rotated_elv_array)
     for row in range(0, rotated_elv_array.shape[0]):
-        _shade_row(shadows[row], rotated_elv_array[row], solar_vec[4], pixel_scale_m, no_data, fuzz=10.0)
+        _shade_row(
+            shadows[row],
+            rotated_elv_array[row],
+            solar_vec[4],
+            pixel_scale_m,
+            no_data,
+            fuzz=10.0,
+        )
 
     del rotated_elv_array
     del buff_elv_array
 
-    shadows = ndimage.interpolation.rotate(shadows, -rot_degrees, reshape=False, output=numpy.float32, cval=no_data,
-                                           prefilter=False)
+    shadows = ndimage.interpolation.rotate(
+        shadows,
+        -rot_degrees,
+        reshape=False,
+        output=numpy.float32,
+        cval=no_data,
+        prefilter=False,
+    )
 
     dr = (shadows.shape[0] - y_size) // 2
     dc = (shadows.shape[1] - x_size) // 2
 
-    shadows = shadows[dr:dr + y_size, dc:dc + x_size]
-    shadows = xarray.DataArray(shadows.reshape(tile.elevation.shape), coords=tile.elevation.coords)
+    shadows = shadows[dr : dr + y_size, dc : dc + x_size]
+    shadows = xarray.DataArray(
+        shadows.reshape(tile.elevation.shape), coords=tile.elevation.coords
+    )
 
     return shadows, slope, sia
